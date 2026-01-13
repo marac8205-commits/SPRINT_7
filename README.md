c3e8f70c247afad152e81b8ff04f5b49c3f22880
unknown <marac8205@gmail.com>
2026-01-12 21:48:06 -0600

    proyecto S 7

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
   SPRINT_7
-## Descripcion del proyecto
-Aplicación web implementada en streamlit
-- pandas
-- plotly
-- numpy
+# vehicles-web-app
+"Web application for vehicle sales data analysis"
+
+## Descripción del Proyecto
+Aplicación web interactiva permite explorar y analizar datos de venta de vehículos usados a través de gráficos dinámicos.
+
+## Para qué sirve
+La aplicación sirve para:
+- Analizar tendencias en el mercado de vehículos usados
+- Visualizar la distribución del kilometraje de los vehículos
+- Explorar la relación entre el kilometraje y el precio de venta
+
+## Funcionalidad
+La aplicación proporciona:
+- **Histograma interactivo**: Muestra la distribución del odómetro de los vehículos
+- **Gráfico de dispersión**: Visualiza la relación entre kilometraje (odómetro) y precio
+- **Interfaz intuitiva**: Botones interactivos para generar visualizaciones bajo demanda
+
+## Tecnologías Utilizadas
+- Streamlit
+- Plotly Express
+- Pandas
--- app_1.py
+++ app_1.py
@@ -1,17 +1,21 @@
+import pandas as pd
 import streamlit as st
-import seaborn as sns
-import yfinance as yf
 import plotly.express as px
 
-df=sns.load_dataset("iris")
-st.title("app nueva version")
-st.write("viva mexico")
-name=st.text_input("Ingresa tu nombre")
-if name:
-    st.write(f"Hola {name}")
-st.dataframe(df)
-nombre_del_activo = st.text_input("Ingresa el symbol (e.g., NVDA, BTC-USD)", "NVDA")
-stock = yf.Ticker(nombre_del_activo)
-df_stocks = stock.history(period="1y")
-fig = px.line(df_stocks)
-st.plotly_chart(fig)
+car_data = pd.read_csv('vehicles_us.csv')  
+
+st.header('Datos de vehículos usados')
+
+hist_button = st.button('Construir histograma')
+        
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Construir gráfico de dispersión')
+        
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
